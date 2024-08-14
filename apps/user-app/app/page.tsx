6562ab38fb5d82dc40e00ac43beec11a222c4a8c
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-08-14 08:08:26 +0530

    init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import { Button } from '@repo/ui/button';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-red-400 text-2xl'>Hello world</div>
+    </>
+  );
+}
